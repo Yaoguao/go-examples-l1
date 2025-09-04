a92d493585ac4d16db02a5cbdb2a07d7736591f6
Yaoguao <glenioJ@yandex.ru>
2025-09-04 22:00:44 +0400

    add task19

---
+++ task19/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+//	TASK 19
+
+/*
+Разворот строки
+Разработать программу, которая переворачивает подаваемую на вход строку.
+
+Например: при вводе строки «главрыба» вывод должен быть «абырвалг».
+
+Учтите, что символы могут быть в Unicode (русские буквы, emoji и пр.),
+то есть просто iterating по байтам может не подойти — нужен срез рун ([]rune).
+*/
+
+func Reverse(str string) string {
+	runes := []rune(str)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func main() {
+	fmt.Println(Reverse("режнессеМ")) // Мессенжер
+}
